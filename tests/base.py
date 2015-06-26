3a80527b29408db222fe8093f141ab54144cd822
Thomas O'Donnell <andy.tom@gmail.com>
2015-06-27 00:10:33 +0100

    Starting to add to the tests

--- tests/base.py
+++ tests/base.py
@@ -7,8 +7,9 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         app.config.from_object('config.TestConfig')
         self.app = app.test_client()
-        db.create_all()
+        self.db = db
+        self.db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        self.db.session.remove()
+        self.db.drop_all()
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,10 +2,6 @@ from base import BaseTestCase
 
 
 class SimpleTestCase(BaseTestCase):
-    def test_index(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status_code, 200)
-
     def test_create_a_snippet(self):
         rv = self.app.get('/new')
         self.assertEqual(rv.status_code, 200)
@@ -14,10 +10,6 @@ class SimpleTestCase(BaseTestCase):
         rv = self.app.get('/snippet')
         # Should redirect to '/'
         self.assertEqual(rv.status_code, 301)
-
-    def test_404_on_missing(self):
-        rv = self.app.get('/snippet/fake')
-        self.assertEqual(rv.status_code, 404)
 
     def test_405_on_get_search(self):
         rv = self.app.get('/search')
