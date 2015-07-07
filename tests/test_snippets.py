8778ba78df6f5d2ec8fd420ffc007127c0f84ffe
Thomas O'Donnell <andy.tom@gmail.com>
2015-07-07 23:49:04 +0100

    Moving files into different files.

--- tests/test_snippets.py
+++ tests/test_snippets.py
@@ -33,29 +33,6 @@ class NoSnippetsTestCase(BaseTestCase):
         self.assertIn("There are no snippets.", rv.data)
 
 
-class ValidationTestCase(BaseTestCase):
-    def test_create_snippet_no_title(self):
-        data = {'text': 'Test Text'}
-
-        rv = self.app.post('/new', data=data)
-
-        self.assertIn('This field is required.', rv.data)
-
-
-    def test_create_snippet_no_text(self):
-        data = {'title': 'Test Title'}
-
-        rv = self.app.post('/new', data=data)
-
-        self.assertIn('This field is required.', rv.data)
-
-    def test_create_snippet_blank(self):
-        rv = self.app.post('/new', data={})
-
-        count = rv.data.count('This field is required.')
-        self.assertEqual(count, 2)
-
-
 class SnippetTestCase(BaseTestCase):
     @unittest.skip("Need to Mock out ES")
     def test_create_snippet(self):
